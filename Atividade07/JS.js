877fac784912d67ae196c62da22453eb05633fd5
Hibrael <hibraelxavier@gmail.com>
2025-04-06 15:57:13 -0300

    atualização

--- Atividade07/JS.js
+++ Atividade07/JS.js
@@ -4,14 +4,46 @@ a.addEventListener("click", mudarcor);
 let impr = document.querySelector("#Enviar")
 impr.addEventListener("click", imprima)
 
+let sm = document.querySelector("#mais")
+sm.addEventListener("click", somar)
+
+let dm = document.querySelector("#menos")
+dm.addEventListener("click", subtrair)
+
+let alterarcor = document.querySelector("#send")
+alterarcor.addEventListener("click", cor)
+
+
 
 function mudarcor(){
     let cor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 document.body.style.background= cor;
+
+    let cor1 = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+document.getElementById("h1master").style.color = cor1
 }
 
 function imprima(){
     let text = document.querySelector("#textbox").value;
     document.getElementById("resultado").innerText = text;
     
+}
+
+function somar(){
+    let contador = document.querySelector("#contador");
+    let contagem = parseFloat(contador.innerText);
+    contagem += 1;
+    contador.innerText = contagem
+}
+
+function subtrair(){
+    let contador = document.querySelector("#contador");
+    let contagem = parseFloat(contador.innerText);
+    contagem -= 1;
+    contador.innerText = contagem
+}
+
+function cor(){
+    let corusuario = document.querySelector("#corusuario").value;
+    document.body.style.backgroundColor = corusuario;
 }
